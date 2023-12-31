6daf511d7b85d04623ac9c444c30d272df4922bc
Prohack101010 <126958051+Prohack101010@users.noreply.github.com>
2023-12-31 22:47:34 +0300

    Update TitleState.hx

--- source/funkin/menus/TitleState.hx
+++ source/funkin/menus/TitleState.hx
@@ -45,9 +45,6 @@ class TitleState extends MusicBeatState
 	override public function create():Void
 	#if android
 FlxG.android.preventDefaultKeys = [BACK];
-#end
-#if android || FlxG.android.justReleased.BACK #end
-SUtil.getStorageDirectory() + 
 	{
 		curWacky = FlxG.random.getObject(getIntroTextShit());
 
