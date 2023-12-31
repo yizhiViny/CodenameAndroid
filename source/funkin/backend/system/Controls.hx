c6c4279f3f2fb0a7dba3f51369315cb18f0a46d2
Prohack101010 <126958051+Prohack101010@users.noreply.github.com>
2023-12-31 21:37:37 +0300

    Update Controls.hx

--- source/funkin/backend/system/Controls.hx
+++ source/funkin/backend/system/Controls.hx
@@ -9,6 +9,11 @@ import flixel.input.actions.FlxActionSet;
 import flixel.input.gamepad.FlxGamepadButton;
 import flixel.input.gamepad.FlxGamepadInputID;
 import flixel.input.keyboard.FlxKey;
+#if mobileC
+import mobile.flixel.FlxButton;
+import mobile.flixel.FlxHitbox;
+import mobile.flixel.FlxVirtualPad;
+#end
 
 enum abstract Action(String) to String from String {
 	var UP = "up";
